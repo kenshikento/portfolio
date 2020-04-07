f0147cdef14b232a2a995804d22bb43de6af88de
kenshikento <tien-nguyen1@hotmail.co.uk>
2020-04-07 15:25:24 +0100

    added in folders for be and fe

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="container">
+                <a> how how </a>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/portfolio">Portfolio</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
